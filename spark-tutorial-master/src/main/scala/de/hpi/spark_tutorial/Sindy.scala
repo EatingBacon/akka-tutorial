c7065c45d902319d4cea05aaaa5ec4e4ea61a189
Tim Garrels <33292335+EatingBacon@users.noreply.github.com>
2021-07-06 13:46:23 +0200

    Solve Homework Number 2 - IND Implementation

--- spark-tutorial-master/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ spark-tutorial-master/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -2,10 +2,45 @@ package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable.ArrayBuffer
+
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
 
-    // TODO
+    // Split Input Dataframes in a set of (value, Array(columnName)) tuples
+    // Should be solvable by collect set and explode as well, which would probably be faster
+    var ds = spark.emptyDataset[(String, Array[String])]
+    for (input <- inputs) {
+      val csv_file = spark.read.format("csv").option("header", "true").option("delimiter", ";").load(input)
+      val columns = csv_file.columns.map(col => Array(col)) // .map(col => Array(input + col))  // Necessary if col names overlap
+      val value_column_name_tuples = csv_file.flatMap(row =>  {
+        row.toSeq.map(_.toString).zip(columns)
+      })
+      ds = ds.union(value_column_name_tuples)
+    }
+
+    val x = ds.rdd
+      // pre aggregation by key, each key now has all its occurrence column names
+      .reduceByKey((v1: Array[String], v2: Array[String]) => v1.union(v2).distinct)
+      //.partitionBy(1)  // We should be able to partition via key, however the index partitioning does not work
+      // Just assume that spark does the repartitioning work for us
+      // create inclusion lists
+      .map(tuple => ArrayBuffer(tuple._2: _*))
+      .flatMap(attributeSet => attributeSet.map(entry => (entry, attributeSet - entry)))
+      // Here could be an partition again (see above)
+      // Aggregate by intersection
+      .reduceByKey((v1: ArrayBuffer[String], v2 : ArrayBuffer[String]) => v1.intersect(v2))
+      // Create output string
+      .map(tuple => {
+        val delim = ", "
+        s"${tuple._1} < ${tuple._2.mkString(delim)}"
+      })
+      // Sort by output string
+      .sortBy(s => s)
+      // Print stuff
+      .collect()
+      .foreach(println)
   }
 }
